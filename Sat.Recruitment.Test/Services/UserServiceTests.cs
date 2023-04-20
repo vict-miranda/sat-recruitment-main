4bc76a976cb1b30f5a68c511a952c3c0e0d0a790
vict-miranda <54920837+vict-miranda@users.noreply.github.com>
2023-04-20 10:01:12 -0400

    add missing tests

---
+++ Sat.Recruitment.Test/Services/UserServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using Sat.Recruitment.Api.Models;
+using Sat.Recruitment.Api.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace Sat.Recruitment.Test.Services
+{
+    [CollectionDefinition("Tests", DisableParallelization = true)]
+    public class UserServiceTests
+    {
+        private User MockUser = new User
+        {
+            Name = "Mike",
+            Email = "mike@gmail.com",
+            Address = "Av. Juan G",
+            Phone = "+349 1122354215",
+            UserType = "Normal",
+            Money = 124
+        };
+
+        [Fact]
+        public async Task ValidateUserDuplicity_ReturnsFalse()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(false);
+            userService.Setup(x => x.ReadUsersFromFile()).ReturnsAsync(new List<User>());
+
+            var result = await userService.Object.ValidateDuplicity(MockUser);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ValidateUserDuplicity_ReturnsTrue()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(true);
+            userService.Setup(x => x.ReadUsersFromFile()).ReturnsAsync(new List<User>());
+
+            var result = await userService.Object.ValidateDuplicity(MockUser);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task ReadUsersFromFile_ReturnsOk()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ReadUsersFromFile()).ReturnsAsync(new List<User> { MockUser });
+
+            var result = await userService.Object.ReadUsersFromFile();
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.True(result.Where(x => x.Name == "Mike").Count() > 0);
+        }
+    }
+}
---
+++ Sat.Recruitment.Test/Services/ValidationServices/UserValidateErrorsTests.cs
@@ -0,0 +1,117 @@
+﻿using Moq;
+using Sat.Recruitment.Api.Models;
+using Sat.Recruitment.Api.Services.Interfaces;
+using Sat.Recruitment.Api.Services.ValidationServices;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sat.Recruitment.Test.Services.ValidationServices
+{
+    [CollectionDefinition("Tests", DisableParallelization = true)]
+    public class UserValidateErrorsTests
+    {
+        private User MockUser = new User
+        {
+            Name = "Mike",
+            Email = "mike@gmail.com",
+            Address = "Av. Juan G",
+            Phone = "+349 1122354215",
+            UserType = "Normal",
+            Money = 124
+        };
+
+        [Fact]
+        public async Task ValidateUser_ReturnsOk()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(false);
+
+            var usersValidateErrors = new UserValidateErrors(userService.Object);
+
+            var result = await usersValidateErrors.ValidateErrors(MockUser);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count == 0);
+        }
+
+        [Fact]
+        public async Task ValidateUser_ReturnsDuplicityError()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(true);
+
+            var usersValidateErrors = new UserValidateErrors(userService.Object);
+            MockUser.Name = string.Empty;
+
+            var result = await usersValidateErrors.ValidateErrors(MockUser);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.True(result.Contains("The user is duplicated"));
+        }
+
+        [Fact]
+        public async Task ValidateUser_ReturnsNameError()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(false);
+
+            var usersValidateErrors = new UserValidateErrors(userService.Object);
+            MockUser.Name = string.Empty;
+
+            var result = await usersValidateErrors.ValidateErrors(MockUser);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.True(result.Contains("The name is required"));
+        }
+
+        [Fact]
+        public async Task ValidateUser_ReturnsEmailError()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(false);
+
+            var usersValidateErrors = new UserValidateErrors(userService.Object);
+            MockUser.Email = string.Empty;
+
+            var result = await usersValidateErrors.ValidateErrors(MockUser);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.True(result.Contains("The email is required"));
+        }
+
+        [Fact]
+        public async Task ValidateUser_ReturnsAddressError()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(false);
+
+            var usersValidateErrors = new UserValidateErrors(userService.Object);
+            MockUser.Address = string.Empty;
+
+            var result = await usersValidateErrors.ValidateErrors(MockUser);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.True(result.Contains("The address is required"));
+        }
+
+        [Fact]
+        public async Task ValidateUser_ReturnsPhoneError()
+        {
+            var userService = new Mock<IUserService>();
+            userService.Setup(x => x.ValidateDuplicity(MockUser)).ReturnsAsync(false);
+
+            var usersValidateErrors = new UserValidateErrors(userService.Object);
+            MockUser.Phone = string.Empty;
+
+            var result = await usersValidateErrors.ValidateErrors(MockUser);
+
+            Assert.NotNull(result);
+            Assert.True(result.Count > 0);
+            Assert.True(result.Contains("The phone is required"));
+        }
+    }
+}
