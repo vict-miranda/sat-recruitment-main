b2dce473fb795e63d83eef7a321c9f2aa6d4bfb0
vict-miranda <54920837+vict-miranda@users.noreply.github.com>
2023-04-20 10:27:03 -0400

    fix money function

--- Sat.Recruitment.Api/Utils/Utils.cs
+++ Sat.Recruitment.Api/Utils/Utils.cs
@@ -22,24 +22,24 @@ namespace Sat.Recruitment.Api.Utils
                     {
                         var percentage = Convert.ToDecimal(0.12);
                         //If new user is normal and has more than USD100
-                        money = money + (newUserMoney * percentage);
+                        money = newUserMoney + (newUserMoney * percentage);
                     }
                     else if (newUserMoney < 100 && newUserMoney > 10)
                     {
                         var percentage = Convert.ToDecimal(0.8);
-                        money = money + (newUserMoney * percentage);
+                        money = newUserMoney + (newUserMoney * percentage);
                     }
                     break;
                 case "SuperUser":
                     if (newUserMoney > 100)
                     {
                         var percentage = Convert.ToDecimal(0.20);
-                        money = money + (newUserMoney * percentage);
+                        money = newUserMoney + (newUserMoney * percentage);
                     }
                     break;
                 case "Premium":
                     if (newUserMoney > 100)
-                        money = money + (newUserMoney * 2);
+                        money = newUserMoney + (newUserMoney * 2);
                     break;
                 default:
                     break;
